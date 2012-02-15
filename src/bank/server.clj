0125f548fd7e4af666cc841280b5761deb0312e4
Jonathan Badger <jhbadger@gmail.com>
2012-02-15 06:24:37 -0800

    got server to start by restructuring code to fit noir framework

--- src/bank/server.clj
+++ src/bank/server.clj
@@ -1,39 +1,10 @@
 (ns bank.server
-  (:use     (noir.core) (hiccup.core) (hiccup.form-helpers) (hiccup.page-helpers))
-  (:require [noir.server :as server] )
-)
+  (:require [noir.server :as server]))
 
-(defpartial signup_form []
-  (label "holder_name" "Your Name:"))
-  
-(defpage "/" []
-  (html5
-    [:head ""]
-    [:body
-      (signup_form)]
-  )
-)
-
-
-(noir.core/defpage "/echo_post" []
-  (html
-    [:html
-      [:head ""]
-      [:body
-        [:p "hi"]
-        [:ul
-          [:li "a thing"]
-          [:li "another thing"]
-          [:li "the last thing"]
-        ]]
-    ]
-  ))
+(server/load-views "src/bank/views/")
 
 (defn -main [& m]
-  (server/start 8080 {:ns `bank.server}))
-; 
-; (defn -main [& m]
-;   (let [mode (keyword (or (first m) :dev))
-;         port (Integer. (get (System/getenv) "PORT" "8080"))]
-;     (server/start port {:mode mode
-;                         :ns `noir-bak})))
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (server/start port {:mode mode
+                        :ns 'bank.server})))
